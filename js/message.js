87b1935ee5a604fed1f427b242bffc202cb2e70c
BasiraCheng <2249639871@qq.com>
2019-05-30 13:40:26 +0800

    修复bug

--- js/message.js
+++ js/message.js
@@ -1,19 +1,21 @@
 !function () {
     var view = document.querySelector('section.message')
+    
     var model = {
         init: function () {
-            var APP_ID = 'IYUDBJIQl5x0amh9G0CbRNOY-gzGzoHsz';
-            var APP_KEY = 'lVOxF7cGmvxyCIy6oUiFpcWI';
-            AV.init({appId: APP_ID, appKey: APP_KEY})
+            var APP_ID = 'v1bWSRjLy4xNSdyAbHLHeYCU-gzGzoHsz';
+            var APP_KEY = 'czImqANFMm3D8qklozrOsML0';
+
+            AV.init({ appId: APP_ID, appKey: APP_KEY });
         },
-        fetch: function () {
+        fetch: function(){
             var query = new AV.Query('Message');
             return query.find()
         },
-        save: function (name, content) {
+        save: function(name,content){
             var Message = AV.Object.extend('Message');
             var message = new Message();
-            return message.save({
+            message.save({
                 'name': name,
                 'content': content
             })
@@ -23,49 +25,47 @@
         view: null,
         model: null,
         messageList: null,
-        init: function (view, model) {
+        init: function (view,model) {
             this.view = view
             this.model = model
-
             this.messageList = view.querySelector('#messageList')
             this.form = view.querySelector('form')
             this.model.init()
             this.loadMessages()
             this.bindEvents()
         },
-
         loadMessages: function () {
-            this.model.fetch().then(
-                (messages) => {
-                    let array = messages.map((item => item.attributes))
-                    array.forEach((item) => {
-                        console.log('here')
-                        let li = document.createElement('li')
-                        li.innerText = `${item.name}: ${item.content}`
-                        this.messageList.appendChild(li)
-                    })
-                }
-            )
+            this.model.fetch().then((messages) => {
+                let array = messages.map((item => item.attributes))
+                array.forEach((item) => {
+                    let li = document.createElement('li')
+                    li.innerText = `${item.name}: ${item.content}`
+                    let messageList = document.querySelector('#messageList')
+                    messageList.appendChild(li)
+
+                })
+            })
         },
         bindEvents: function () {
             this.form.addEventListener('submit', (e) => {
                 e.preventDefault()
                 this.saveMessage()
+
             })
         },
         saveMessage: function () {
             let myForm = this.form
             let content = myForm.querySelector('input[name=content]').value
             let name = myForm.querySelector('input[name=name]').value
-            this.model.save(name.content).then(function (object) {
+            this.model.save(name,content).then(function (object) {
                 let li = document.createElement('li')
                 li.innerText = `${object.attributes.name}: ${object.attributes.content}`
                 let messageList = document.querySelector('#messageList')
                 messageList.appendChild(li)
-                myForm.querySelector('input[name=content]').value = ''
-                console.log(obiect)
+                myForm.querySelector('input[name=name]').value = ''
+                console.log(object)
             })
         }
     }
-    controller.init(view, model)
+    controller.init(view,model)
 }.call()
